9aff6ece8af1b75e52970f2bc5c1b64c6a532bd1
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-03-06 19:12:54 +0200

    5

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
     try {
       const response = await fetch(
         // `https://dall-e-tkfk.onrender.com/api/v1/post`,
-        `${process.env.REACT_APP_API_URL}/api/v1/post`,
+        process.env.REACT_APP_API_URL + `/api/v1/post`,
         {
           method: "GET",
           headers: {
