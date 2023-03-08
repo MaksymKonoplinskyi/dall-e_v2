f27c97d1ba37fb4f2f0f7239ad7fd39cbea649b3
MaksymKonoplinskyi <gran7777777@gmail.com>
2023-03-08 12:55:48 +0200

    1

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -86,13 +86,13 @@ const CreatePost = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        {/* <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1> */}
         <p className="mt-2 text-[#666e75] text-[16px] max-w [500px]">
           Create imaginative and visually stunning images through DALL-E AI and
           share them with the community
         </p>
       </div>
-      <form onSubmit={handleSubmit} className="mt-16 max-w-3xl">
+      <form onSubmit={handleSubmit} className="mt-4 max-w-3xl">
         <div className="flex flex-col gap-5">
           <FormField
             labelName="Your name"
